f86ba9b86af01f76b75a4cab91fbcd888b1d7aab
Pawel Burniak <pawel@burniak.pl>
2016-04-20 14:39:56 +0200

    Use template with common parts of websites Add mean start-end time implementation

--- src/presence_analyzer/views.py
+++ src/presence_analyzer/views.py
@@ -4,13 +4,23 @@ Defines views.
 """
 
 import calendar
-from flask import redirect, abort
+import logging
+from collections import OrderedDict
+
+from flask import redirect, abort, url_for, render_template
 
 from presence_analyzer.main import app
-from presence_analyzer.utils import jsonify, get_data, mean, group_by_weekday
+from presence_analyzer.utils import (
+    jsonify, get_data, mean, group_by_weekday, mean_start_end)
 
-import logging
 log = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+ENDPOINTS = OrderedDict([
+    ('presence_weekday', 'Presence by weekday'),
+    ('mean_time_weekday', 'Presence mean time'),
+    ('presence_start_end', 'Presence start - end')
+])
 
 
 @app.route('/')
@@ -18,7 +28,15 @@ def mainpage():
     """
     Redirects to front page.
     """
-    return redirect('/static/presence_weekday.html')
+    return redirect(url_for('render', site='presence_weekday'))
+
+
+@app.route('/<site>')
+def render(site):
+    if site not in ENDPOINTS:
+        abort(404)
+    return render_template(
+        '{}.html'.format(site), name=site, endpoints=ENDPOINTS)
 
 
 @app.route('/api/v1/users', methods=['GET'])
@@ -72,4 +90,23 @@ def presence_weekday_view(user_id):
     ]
 
     result.insert(0, ('Weekday', 'Presence (s)'))
+    return result
+
+
+@app.route('/api/v1/presence_start_end/<int:user_id>', methods=['GET'])
+@jsonify
+def presence_start_end_view(user_id):
+    """
+    Returns mean time of given user grouped by weekday.
+    """
+    data = get_data()
+    if user_id not in data:
+        log.debug('User %s not found!', user_id)
+        abort(404)
+
+    weekdays = mean_start_end(data[user_id])
+    result = [
+        (calendar.day_abbr[weekday], times[0], times[1])
+        for weekday, times in enumerate(weekdays)
+    ]
     return result
